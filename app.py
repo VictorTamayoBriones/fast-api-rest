407fdd3ff3e51ce2402e47f36c256dcb9c925295
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-02-01 23:43:26 -0600

    routes

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
+from datetime import datetime
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Text, Optional
+from uuid import uuid4 as uuid
 
 app = FastAPI()
+
+posts = []
+
+#Post Model
+
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author:str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: Optional[datetime]
+    published: bool = False
+
+@app.get('/')
+def read_root():
+    return {"welcome":"Welcome to my REST API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/post')
+def save_post(post: Post):
+    post.id = str(uuid())
+    posts.append(post.dict())
+    return "recived"
